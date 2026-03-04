182d4d63ec16e4512aedfe18ba24ff4d886d835c
kolmipilot <92938734+kolmipilot@users.noreply.github.com>
2026-03-04 18:27:19 +0100

    rhs_usaf ready

--- addons/compat_rhs_usf/Helmets.hpp
+++ addons/compat_rhs_usf/Helmets.hpp
@@ -99,6 +99,30 @@ class H_HelmetB: ItemCore {
 
 
 RHS_BASECAP(rhsusf_patrolcap_ocp,H_HelmetB);
+class rhs_xmas_antlers : ItemCore
+{
+	class ItemInfo: HeadgearItem
+	{
+		ace_hearing_protection = 1;
+		ace_hearing_lowerVolume = 0.0;
+		mass = 25;
+		class HitpointsProtectionInfo
+		{
+			class Head
+			{
+				hitpointName="HitHead";
+				Armor=8;
+				passThrough = 0.5;
+			};
+			class Face
+			 {
+				hitpointName="HitFace";
+				armor=8;
+				passThrough=0.5;
+			};
+		};
+	};
+};
 RHS_BASECAP(rhs_Booniehat_ocp,ItemCore);
 RHS_BASECAP(rhs_booniehat2_marpatd,ItemCore);
 RHS_BASECAP(rhs_8point_marpatd,ItemCore);
@@ -106,4 +130,70 @@ RHS_BASECAP(rhs_8point_marpatd,ItemCore);
 RHS_HELMET_LIGHT(rhsusf_ach_helmet_ocp,H_HelmetB);
 RHS_HELMET_LIGHT(rhsusf_opscore_01,rhsusf_ach_helmet_ocp);
 RHS_HELMET_LIGHT(rhsusf_opscore_cover,rhsusf_opscore_01);
-RHS_HELMET_LIGHT(rhsusf_opscore_mar_01,rhsusf_ach_helmet_ocp)
+RHS_HELMET_LIGHT(rhsusf_opscore_mar_01,rhsusf_ach_helmet_ocp);
+
+RHS_HELMET_MEDIUM(rhsusf_cvc_helmet,rhsusf_opscore_01);
+
+RHS_HELMET_HEAVY(rhsusf_hgu56p,rhsusf_opscore_01);
+
+RHS_HELMET_MEDIUM(rhsusf_mich_helmet_marpatwd,H_HelmetB);
+
+class rhsusf_mich_helmet_marpatwd_norotos_arc : rhsusf_mich_helmet_marpatwd
+{
+	class ItemInfo : ItemInfo{};
+};
+class rhsusf_mich_bare : rhsusf_mich_helmet_marpatwd
+{
+	class ItemInfo : ItemInfo{};
+};
+class rhsusf_mich_bare_headset : rhsusf_mich_bare
+{
+	class ItemInfo : ItemInfo{};
+};
+
+RHS_HELMET_MEDIUM(rhsusf_mich_bare_alt,rhsusf_mich_bare);
+
+class rhsusf_mich_bare_norotos : rhsusf_mich_bare
+{
+	class ItemInfo : ItemInfo{};
+};
+class rhsusf_mich_bare_norotos_headset : rhsusf_mich_bare_headset
+{
+	class ItemInfo : ItemInfo{};
+};
+
+RHS_HELMET_MEDIUM(rhsusf_mich_bare_norotos_alt,rhsusf_mich_bare_norotos);
+RHS_HELMET_MEDIUM(rhsusf_mich_bare_norotos_alt_headset,rhsusf_mich_bare_norotos_headset);
+
+class rhsusf_mich_bare_norotos_arc : rhsusf_mich_bare
+{
+	class ItemInfo : ItemInfo{};
+};
+class rhsusf_mich_bare_norotos_arc_headset : rhsusf_mich_bare_headset
+{
+	class ItemInfo : ItemInfo{};
+};
+
+RHS_HELMET_MEDIUM(rhsusf_mich_bare_norotos_arc_alt,rhsusf_mich_bare_norotos_arc);
+RHS_HELMET_MEDIUM(rhsusf_mich_bare_norotos_arc_alt_headset,rhsusf_mich_bare_norotos_arc_headset);
+RHS_HELMET_MEDIUM(rhsusf_mich_bare_tan,rhsusf_mich_bare);
+RHS_HELMET_MEDIUM(rhsusf_mich_bare_tan_headset,rhsusf_mich_bare_headset);
+RHS_HELMET_MEDIUM(rhsusf_mich_bare_semi,rhsusf_mich_bare);
+RHS_HELMET_MEDIUM(rhsusf_mich_bare_semi_headset,rhsusf_mich_bare_headset);
+
+class rhsusf_mich_bare_norotos_semi : rhsusf_mich_bare_semi
+{
+	class ItemInfo : ItemInfo{};
+};
+
+RHS_HELMET_MEDIUM(rhsusf_mich_bare_norotos_semi_headset,rhsusf_mich_bare_semi_headset);
+RHS_HELMET_MEDIUM(rhsusf_mich_bare_norotos_alt_semi,rhsusf_mich_bare_norotos_semi);
+RHS_HELMET_MEDIUM(rhsusf_lwh_helmet_marpatd,H_HelmetB);
+
+RHS_BASECAP(rhsusf_Bowman,ItemCore);
+RHS_BASECAP(rhsusf_bowman_cap,rhsusf_Bowman);
+
+RHS_HELMET_HEAVY(RHS_jetpilot_usaf,H_HelmetB);
+RHS_HELMET_HEAVY(rhsusf_ihadss,rhsusf_opscore_01);
+
+RHS_HELMET_LIGHT(rhsusf_protech_helmet,H_HelmetB);
---
+++ addons/compat_rhs_usf/Vests.hpp
@@ -0,0 +1,133 @@
+// Macro for standard vests without ACE hearing protection (armor 6)
+#define RHS_VEST_MEDIUM(VestClass,BaseClass) \
+class VestClass: BaseClass { \
+    class ItemInfo: ItemInfo { \
+        class HitpointsProtectionInfo \
+        { \
+            class Neck \
+            { \
+                hitpointName = "HitNeck"; \
+                armor = 0; \
+                passThrough = 1; \
+                simulation = ""; \
+            }; \
+            class Chest \
+            { \
+                hitpointName = "HitChest"; \
+                armor = 12; \
+                passThrough = 0.4; \
+                simulation = ""; \
+            }; \
+            class Arms \
+            { \
+                hitpointName = "HitArms"; \
+                armor = 0; \
+                passThrough = 1; \
+                simulation = ""; \
+            }; \
+            class Diaphragm \
+            { \
+                hitpointName = "HitDiaphragm"; \
+                armor = 12; \
+                passThrough = 0.4; \
+                simulation = ""; \
+            }; \
+            class Abdomen \
+            { \
+                hitpointName = "HitAbdomen"; \
+                armor = 12; \
+                passThrough = 0.4; \
+                simulation = ""; \
+            }; \
+            class Pelvis \
+            { \
+                hitpointName = "HitPelvis"; \
+                armor = 12; \
+                passThrough = 0.4; \
+                simulation = ""; \
+            }; \
+            class Body \
+            { \
+                hitpointName = "HitBody"; \
+                passThrough = 0.4; \
+                simulation = ""; \
+            }; \
+        }; \
+    }; \
+}
+
+class Vest_Camo_Base: ItemCore
+{
+	class ItemInfo;
+};
+
+
+RHS_VEST_MEDIUM(rhsusf_iotv_ocp_base,Vest_Camo_Base);
+RHS_VEST_MEDIUM(rhsusf_iotv_ocp,rhsusf_iotv_ocp_base);
+RHS_VEST_MEDIUM(rhsusf_iotv_ocp_Grenadier,rhsusf_iotv_ocp_base);
+RHS_VEST_MEDIUM(rhsusf_iotv_ocp_Medic,rhsusf_iotv_ocp_base);
+RHS_VEST_MEDIUM(rhsusf_iotv_ocp_Repair,rhsusf_iotv_ocp_base);
+RHS_VEST_MEDIUM(rhsusf_iotv_ocp_Rifleman,rhsusf_iotv_ocp_base);
+RHS_VEST_MEDIUM(rhsusf_iotv_ocp_SAW,rhsusf_iotv_ocp_base);
+RHS_VEST_MEDIUM(rhsusf_iotv_ocp_Squadleader,rhsusf_iotv_ocp_base);
+RHS_VEST_MEDIUM(rhsusf_iotv_ocp_Teamleader,rhsusf_iotv_ocp_base);
+RHS_VEST_MEDIUM(rhsusf_iotv_ucp_base,rhsusf_iotv_ocp_base);
+RHS_VEST_MEDIUM(rhsusf_iotv_ucp,rhsusf_iotv_ocp);
+RHS_VEST_MEDIUM(rhsusf_iotv_ucp_Grenadier,rhsusf_iotv_ucp_base);
+RHS_VEST_MEDIUM(rhsusf_iotv_ucp_Medic,rhsusf_iotv_ucp_base);
+RHS_VEST_MEDIUM(rhsusf_iotv_ucp_Repair,rhsusf_iotv_ucp_base);
+RHS_VEST_MEDIUM(rhsusf_iotv_ucp_Rifleman,rhsusf_iotv_ucp_base);
+RHS_VEST_MEDIUM(rhsusf_iotv_ucp_SAW,rhsusf_iotv_ucp_base);
+RHS_VEST_MEDIUM(rhsusf_spc,rhsusf_iotv_ocp_base);
+RHS_VEST_MEDIUM(rhsusf_spc_rifleman,rhsusf_spc);
+RHS_VEST_MEDIUM(rhsusf_iotv_ucp_Squadleader,rhsusf_iotv_ucp_base);
+RHS_VEST_MEDIUM(rhsusf_spc_iar,rhsusf_spc_rifleman);
+RHS_VEST_MEDIUM(rhsusf_iotv_ucp_Teamleader,rhsusf_iotv_ucp_base);
+RHS_VEST_MEDIUM(rhsusf_spc_corpsman,rhsusf_spc_rifleman);
+RHS_VEST_MEDIUM(rhsusf_spc_crewman,rhsusf_spc_rifleman);
+RHS_VEST_MEDIUM(rhsusf_spc_light,rhsusf_spc_rifleman);
+RHS_VEST_MEDIUM(rhsusf_spc_marksman,rhsusf_spc_rifleman);
+RHS_VEST_MEDIUM(rhsusf_spc_mg,rhsusf_spc_rifleman);
+RHS_VEST_MEDIUM(rhsusf_spc_sniper,rhsusf_spc_rifleman);
+RHS_VEST_MEDIUM(rhsusf_spc_squadleader,rhsusf_spc_rifleman);
+RHS_VEST_MEDIUM(rhsusf_spc_teamleader,rhsusf_spc_rifleman);
+RHS_VEST_MEDIUM(rhsusf_spc_patchless,rhsusf_spc_light);
+RHS_VEST_MEDIUM(rhsusf_spc_patchless_radio,rhsusf_spc_squadleader);
+RHS_VEST_MEDIUM(rhsusf_spcs_ocp,rhsusf_iotv_ocp_base);
+RHS_VEST_MEDIUM(rhsusf_spcs_ocp_squadleader,rhsusf_spcs_ocp);
+RHS_VEST_MEDIUM(rhsusf_spcs_ocp_teamleader,rhsusf_spcs_ocp);
+RHS_VEST_MEDIUM(rhsusf_spcs_ocp_teamleader_alt,rhsusf_spcs_ocp);
+RHS_VEST_MEDIUM(rhsusf_spcs_ocp_saw,rhsusf_spcs_ocp);
+RHS_VEST_MEDIUM(rhsusf_spcs_ocp_grenadier,rhsusf_spcs_ocp);
+RHS_VEST_MEDIUM(rhsusf_spcs_ocp_rifleman,rhsusf_spcs_ocp);
+RHS_VEST_MEDIUM(rhsusf_spcs_ocp_rifleman_alt,rhsusf_spcs_ocp);
+RHS_VEST_MEDIUM(rhsusf_spcs_ocp_medic,rhsusf_spcs_ocp);
+RHS_VEST_MEDIUM(rhsusf_spcs_ocp_crewman,rhsusf_spcs_ocp);
+RHS_VEST_MEDIUM(rhsusf_spcs_ocp_machinegunner,rhsusf_spcs_ocp);
+RHS_VEST_MEDIUM(rhsusf_spcs_ocp_sniper,rhsusf_spcs_ocp);
+RHS_VEST_MEDIUM(rhsusf_spcs_ucp,rhsusf_spcs_ocp);
+RHS_VEST_MEDIUM(rhsusf_spcs_ucp_squadleader,rhsusf_spcs_ocp_squadleader);
+RHS_VEST_MEDIUM(rhsusf_spcs_ucp_teamleader,rhsusf_spcs_ocp_teamleader);
+RHS_VEST_MEDIUM(rhsusf_spcs_ucp_teamleader_alt,rhsusf_spcs_ocp_teamleader_alt);
+RHS_VEST_MEDIUM(rhsusf_spcs_ucp_saw,rhsusf_spcs_ocp_saw);
+RHS_VEST_MEDIUM(rhsusf_spcs_ucp_grenadier,rhsusf_spcs_ocp_grenadier);
+RHS_VEST_MEDIUM(rhsusf_spcs_ucp_rifleman,rhsusf_spcs_ocp_rifleman);
+RHS_VEST_MEDIUM(rhsusf_spcs_ucp_rifleman_alt,rhsusf_spcs_ocp_rifleman_alt);
+RHS_VEST_MEDIUM(rhsusf_spcs_ucp_medic,rhsusf_spcs_ocp_medic);
+RHS_VEST_MEDIUM(rhsusf_spcs_ucp_crewman,rhsusf_spcs_ocp_crewman);
+RHS_VEST_MEDIUM(rhsusf_spcs_ucp_machinegunner,rhsusf_spcs_ocp_machinegunner);
+RHS_VEST_MEDIUM(rhsusf_spcs_ucp_sniper,rhsusf_spcs_ocp_sniper);
+RHS_VEST_MEDIUM(rhsusf_mbav,rhsusf_iotv_ocp_base);
+RHS_VEST_MEDIUM(rhsusf_mbav_light,rhsusf_mbav);
+RHS_VEST_MEDIUM(rhsusf_mbav_rifleman,rhsusf_mbav_light);
+RHS_VEST_MEDIUM(rhsusf_mbav_mg,rhsusf_mbav_light);
+RHS_VEST_MEDIUM(rhsusf_mbav_grenadier,rhsusf_mbav_light);
+RHS_VEST_MEDIUM(rhsusf_mbav_medic,rhsusf_mbav_light);
+RHS_VEST_MEDIUM(rhsusf_plateframe_sapi,rhsusf_iotv_ocp_base);
+RHS_VEST_MEDIUM(rhsusf_plateframe_light,rhsusf_plateframe_sapi);
+RHS_VEST_MEDIUM(rhsusf_plateframe_rifleman,rhsusf_plateframe_sapi);
+RHS_VEST_MEDIUM(rhsusf_plateframe_teamleader,rhsusf_plateframe_rifleman);
+RHS_VEST_MEDIUM(rhsusf_plateframe_grenadier,rhsusf_plateframe_rifleman);
+RHS_VEST_MEDIUM(rhsusf_plateframe_machinegunner,rhsusf_plateframe_rifleman);
+RHS_VEST_MEDIUM(rhsusf_plateframe_marksman,rhsusf_plateframe_rifleman);
+RHS_VEST_MEDIUM(rhsusf_plateframe_medic,rhsusf_plateframe_rifleman);
