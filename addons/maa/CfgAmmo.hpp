d1d46eceaa0a3cd963f80c7b57550ae78560c222
kolmipilot <92938734+kolmipilot@users.noreply.github.com>
2026-08-07 23:06:52 +0200

    maa

---
+++ addons/maa/CfgAmmo.hpp
@@ -0,0 +1,192 @@
+#define AddTracerRed model="\A3\Weapons_f\Data\bullettracer\tracer_red"
+#define AddTracerGreen model="\A3\Weapons_f\Data\bullettracer\tracer_green"
+#define AddTracerYellow model="\A3\Weapons_f\Data\bullettracer\tracer_yellow"
+#define AddTracerWhite model="\A3\Weapons_f\Data\bullettracer\tracer_white"
+
+class CfgAmmo
+{    
+    class M_Titan_AT;
+    class M_Titan_AP;
+    class ammo_Penetrator_MRAAWS;
+    class ammo_Penetrator_MRAAWS_HEAT55;
+    class R_MRAAWS_HE_F;
+    class R_MRAAWS_HEAT55_F;
+    
+    class MAA_M_GMM_HEAT: M_Titan_AT
+    {
+        model="\A3\Weapons_F_beta\Launchers\titan\titan_missile_at_fly";
+        hit=150;
+        indirectHit=14;
+        indirectHitRange=3;
+        explosive=1;
+        warheadName="HE";
+        submunitionAmmo="ammo_Penetrator_MRAAWS";
+        submunitionDirectionType="SubmunitionModelDirection";
+        submunitionInitSpeed=1000;
+        submunitionParentSpeedCoef=0;
+        submunitionInitialOffset[]={0,0,-0.2};
+        cost=500;
+        initTime=0.1;
+        maxControlRange=2500;
+    };
+    
+    class MAA_M_GMM_HE: M_Titan_AP
+    {
+        hit=200;
+        indirectHit=50;
+        indirectHitRange=6;
+        warheadName="HE";
+    };
+    
+    class MAA_M_GMM_MT: M_Titan_AT
+    {
+        hit=200;
+        indirectHit=40;
+        indirectHitRange=5;
+        warheadName="HE";
+        submunitionAmmo="MAA_Penetrator_MT756";
+        CraterEffects="ArtyShellCrater";
+        ExplosionEffects="MortarExplosion";
+        explosionEffectsDir="explosionDir";
+    };
+    
+    class MAA_M_ASM509: R_MRAAWS_HE_F
+    {
+        hit=300;
+        indirectHit=50;
+        indirectHitRange=10;
+        warheadName="HE";
+        CraterEffects="ArtyShellCrater";
+        ExplosionEffects="MortarExplosion";
+        explosionEffectsDir="explosionDir";
+    };
+    
+    class MAA_M_HEDP502: R_MRAAWS_HEAT55_F
+    {
+        hit=150;
+        indirectHit=40;
+        caliber = 5;
+        indirectHitRange=6;
+        warheadName="HE";
+        submunitionAmmo="MAA_Penetrator_HEDP502";
+    };
+    
+    class MAA_Penetrator_HEDP502: ammo_Penetrator_MRAAWS_HEAT55
+    {
+        caliber=10;
+        hit=400;
+    };
+    
+    //Can't get the shotgun round to work either :(
+    class MAA_M_ADM401: R_MRAAWS_HE_F
+    {
+        hit=30;
+        explosive=0;
+        caliber=1;
+        indirectHit=0;
+        indirectHitRange=2;
+        deflectionSlowDown = 0.8;
+        directionalExplosion = 0;
+        explosionDir = "explosionDir";
+        explosionEffectsDir = "explosionDir";
+        explosionPos = "explosionPos";
+        fuseDistance = 0;
+        explosionType = "explosive";
+        explosionForceCoef = 1;
+        deleteParentWhenTriggered=1;
+        mineBoundingDist = 3;
+        mineBoundingTime = 3;
+        mineDiveSpeed = 1;
+        mineFloating = -1;
+        mineInconspicuousness = 10;
+        mineJumpEffects = "";
+        minePlaceDist = 0.5;
+        mineTrigger = "RangeTrigger";
+        submunitionAmmo = "B_127x108_Ball";
+        submunitionConeAngle = 20;
+        submunitionConeAngleHorizontal = 50;
+        submunitionConeType[] = {"poissondisccenter",1100};
+        submunitionDirectionType = "SubmunitionModelDirection";
+        submunitionInitialOffset[] = {0,0,0.1};
+        submunitionInitSpeed=0;
+        submunitionParentSpeedCoef = 2.5;
+        triggerTime = 0.01;
+        thrust = 0.1;
+        thrustTime = 0.1;
+        timeToLive = 25;
+        
+    };
+    
+    class MAA_M_ILLUM545: R_MRAAWS_HE_F
+    {
+        hit=30;
+        explosive=0;
+        brightness=120;
+        caliber=1;
+        indirectHit=0;
+        indirectHitRange=2;
+        submunitionAmmo="F_40mm_Yellow";
+        triggerOnImpact=1;
+        ExplosionEffects="";
+        explosionEffectsDir="";
+        deleteParentWhenTriggered=1;
+        triggerTime=0.7;
+        triggerDistance=5;
+        submunitionInitialOffset[]={0,0,0.1};
+        submunitionInitSpeed=1;
+        //submunitionParentSpeedCoef = 1;
+    };
+    
+    class MAA_M_SMOKE469: R_MRAAWS_HE_F
+    {
+        hit=30;
+        explosive=0;
+        caliber=1;
+        indirectHit=0;
+        indirectHitRange=2;
+        submunitionAmmo="G_40mm_Smoke";
+        submunitionConeType[] = {"poissondisccenter",3};
+        submunitionConeAngle = 20;
+        submunitionConeAngleHorizontal = 50;
+        triggerDistance=5;
+        triggerOnImpact=1;
+        ExplosionEffects="";
+        explosionEffectsDir="";
+        deleteParentWhenTriggered=1;
+        submunitionInitSpeed=0;
+    };
+    
+    class MAA_M_MT756: R_MRAAWS_HEAT55_F
+    {
+        hit=200;
+        indirectHit=40;
+        indirectHitRange=5;
+        warheadName="HE";
+        submunitionAmmo="MAA_Penetrator_MT756";
+        CraterEffects="ArtyShellCrater";
+        ExplosionEffects="MortarExplosion";
+        explosionEffectsDir="explosionDir";
+    };
+    
+    class MAA_Penetrator_MT756: ammo_Penetrator_MRAAWS
+    {
+        caliber=40;
+        warheadName="TandemHEAT";
+        hit=600;
+    };
+    
+    class MAA_M_HE441_AB100: R_MRAAWS_HE_F
+    {
+        timeToLive=0.2857;
+    };
+    
+    class MAA_M_HE441_AB250: R_MRAAWS_HE_F
+    {
+        timeToLive=0.71425;
+    };
+    
+    class MAA_M_HE441_AB500: R_MRAAWS_HE_F
+    {
+        timeToLive=1.4285;
+    };
+};
---
+++ addons/maa/CfgMagazines.hpp
@@ -0,0 +1,138 @@
+class CfgMagazines
+{
+    class Vorona_HEAT;
+    class Vorona_HE;
+    class MRAWS_HE_F;
+    class MRAWS_HEAT55_F;
+    
+    class MAA_MAAWS_GMM_HEAT: Vorona_HEAT
+    {
+        author="Neonex";
+        displayName=CSTRING(Name_GMMHEAT);
+        model="\A3\Weapons_F_beta\Launchers\titan\titan_missile_at";
+        ammo="MAA_M_GMM_HEAT";
+        picture="\z\ibc\addons\maa\UI\GMMHEAT.paa";
+        displayNameShort="GMM HEAT";
+        descriptionShort=CSTRING(Desc_GMMHEAT);
+        mass=70;
+    };
+    
+    class MAA_MAAWS_GMM_HE: Vorona_HE
+    {
+        author="Neonex";
+        displayName=CSTRING(Name_GMMHE);
+        model="\A3\Weapons_F_beta\Launchers\titan\titan_missile_ap";
+        ammo="MAA_M_GMM_HE";
+        displayNameShort="GMM HE";
+        picture="\z\ibc\addons\maa\UI\GMMHE.paa";
+        descriptionShort=CSTRING(Desc_GMMHE);
+        mass=50;
+    };
+    
+    class MAA_MAAWS_GMM_MT: Vorona_HEAT
+    {
+        author="Neonex";
+        displayName=CSTRING(Name_GMMMT);
+        model="\A3\Weapons_F_beta\Launchers\titan\titan_missile_atl";
+        ammo="MAA_M_GMM_MT";
+        picture="\z\ibc\addons\maa\UI\GMMMT.paa";
+        displayNameShort="GMM MT";
+        descriptionShort=CSTRING(Desc_GMMMT);
+        mass=100;
+    };
+    
+    class MAA_MAAWS_ILLUM545: MRAWS_HE_F
+    {
+        author="Neonex";
+        displayName=CSTRING(Name_ILLUM545);
+        model="\a3\Weapons_F_Tank\Launchers\MRAWS\rocket_MRAWS_HE_F_item.p3d";
+        ammo="MAA_M_ILLUM545";
+        picture="\z\ibc\addons\maa\UI\ILLUM545.paa";
+        displayNameShort="ILLUM";
+        descriptionShort=CSTRING(Desc_ILLUM545);
+        mass=30;
+    };
+    
+    class MAA_MAAWS_SMOKE469: MRAWS_HE_F
+    {
+        author="Neonex";
+        displayName=CSTRING(Name_SMOKE469);
+        model="\a3\Weapons_F_Tank\Launchers\MRAWS\rocket_MRAWS_HE_F_item.p3d";
+        ammo="MAA_M_SMOKE469";
+        picture="\z\ibc\addons\maa\UI\SMOKE469.paa";
+        displayNameShort="SMOKE";
+        descriptionShort=CSTRING(Desc_SMOKE469);
+        mass=30;
+    };
+    
+    class MAA_MAAWS_ASM509: MRAWS_HE_F
+    {
+        author="Neonex";
+        displayName=CSTRING(Name_ASM509);
+        ammo="MAA_M_ASM509";
+        displayNameShort="ASM";
+        picture="\z\ibc\addons\maa\UI\ASM509.paa";
+        descriptionShort=CSTRING(Desc_ASM509);
+        mass=50;
+    };
+    
+    class MAA_MAAWS_HEDP502: MRAWS_HEAT55_F
+    {
+        author="Neonex";
+        displayName=CSTRING(Name_HEDP502);
+        ammo="MAA_M_HEDP502";
+        displayNameShort="HEDP";
+        picture="\z\ibc\addons\maa\UI\HEDP502.paa";
+        descriptionShort=CSTRING(Desc_HEDP502);
+        mass=40;
+    };
+    
+    class MAA_MAAWS_MT756: MRAWS_HEAT55_F
+    {
+        author="Neonex";
+        displayName=CSTRING(Name_MT756);
+        ammo="MAA_M_MT756";
+        displayNameShort="MT";
+        picture="\z\ibc\addons\maa\UI\MT756.paa";
+        descriptionShort=CSTRING(Desc_MT756);
+        mass=90;
+    };
+    
+    class MAA_MAAWS_ADM401: MRAWS_HE_F
+    {
+        author="Neonex";
+        displayName=CSTRING(Name_ADM401);
+        ammo="MAA_M_ADM401";
+        displayNameShort="ADM";
+        picture="\z\ibc\addons\maa\UI\ADM401.paa";
+        descriptionShort=CSTRING(Desc_ADM401);
+        mass=40;
+    };
+    
+    class MAA_MAAWS_HE441_AB100: MRAWS_HE_F
+    {
+        author="Neonex";
+        displayName=CSTRING(Name_HE441AB100);
+        ammo="MAA_M_HE441_AB100";
+        displayNameShort="Airburst 100 m";
+        descriptionShort=CSTRING(Desc_HE441AB100);
+    };
+    
+    class MAA_MAAWS_HE441_AB250: MRAWS_HE_F
+    {
+        author="Neonex";
+        displayName=CSTRING(Name_HE441AB250);
+        ammo="MAA_M_HE441_AB250";
+        displayNameShort="Airburst 250 m";
+        descriptionShort=CSTRING(Desc_HE441AB250);
+    };
+    
+    class MAA_MAAWS_HE441_AB500: MRAWS_HE_F
+    {
+        author="Neonex";
+        displayName=CSTRING(Name_HE441AB500);
+        ammo="MAA_M_HE441_AB500";
+        displayNameShort="Airburst 500 m";
+        descriptionShort=CSTRING(Desc_HE441AB500);
+    };
+};
