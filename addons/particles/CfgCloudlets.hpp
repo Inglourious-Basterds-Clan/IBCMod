cd47083d431272da9d859bc97e7cd84a1aff8add
MiszczuZPolski <71414303+MiszczuZPolski@users.noreply.github.com>
2026-08-27 18:02:22 +0200

    Update CfgCloudlets.hpp

--- addons/particles/CfgCloudlets.hpp
+++ addons/particles/CfgCloudlets.hpp
@@ -1,8 +1,8 @@
 class CfgCloudlets {
-	class Default;
-	class SmokeShellWhite : Default {
-		moveVelocity[] = {0.2, 0.50000001, 0.1};//{0.2,0.1,0.1}
-		size[] = {0.36, 3.5, 12};//{0.1,2,6}
-		MoveVelocityVar[] = {0.7, 0.4, 0.7};//{0.25,0.25,0.25}
-	};
+    class Default;
+    class SmokeShellWhite : Default {
+        moveVelocity[] = {0.2, 0.50000001, 0.1};//{0.2,0.1,0.1}
+        size[] = {0.36, 3.5, 12};//{0.1,2,6}
+        MoveVelocityVar[] = {0.7, 0.4, 0.7};//{0.25,0.25,0.25}
+    };
 };
