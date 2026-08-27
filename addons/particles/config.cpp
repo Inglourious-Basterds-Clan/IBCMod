b63362890a9e976633eb4e2ba88bb09eae4b3e31
MiszczuZPolski <71414303+MiszczuZPolski@users.noreply.github.com>
2026-08-27 18:03:11 +0200

    Particles - Add new module (#39)
    * particles
    
    * Update CfgCloudlets.hpp
    
    * Update config.cpp
    
    * Update CfgCloudlets.hpp

---
+++ addons/particles/config.cpp
@@ -0,0 +1,15 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        author = "MiszczuZPolski";
+        authors[] = {"MiszczuZPolski"};
+        VERSION_CONFIG;
+        requiredAddons[] = {"ibc_main"};
+        units[] = {};
+        weapons[] = {};
+    };
+};
+
+#include "CfgCloudlets.hpp"
---
+++ addons/particles/script_component.hpp
@@ -0,0 +1,18 @@
+#define COMPONENT particles
+#define COMPONENT_BEAUTIFIED Particles
+
+#include "\z\ibc\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+
+#ifdef DEBUG_ENABLED_PARTICLES
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_PARTICLES
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_PARTICLES
+#endif
+
+#include "\z\ibc\addons\main\script_macros.hpp"
