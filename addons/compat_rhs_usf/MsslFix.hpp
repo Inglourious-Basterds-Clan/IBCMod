e16997630c3a831e782ffbcaabd977d03a98ff1b
kolmipilot <92938734+kolmipilot@users.noreply.github.com>
2026-08-31 13:41:45 +0200

    fix formating again

--- addons/compat_rhs_usf/MsslFix.hpp
+++ addons/compat_rhs_usf/MsslFix.hpp
@@ -11,34 +11,34 @@ class rhs_weap_FFARLauncher: RocketPods {
         "rhs_mag_FFAR_19_USAF",
         "rhs_mag_FFAR_57_USAF_LAU61_3"
         };
-    displayName="Hydra (M151 HE)";
+    displayName = "Hydra (M151 HE)";
     modes[] = {
         "Far_AI",
         "Single",
         "Burst"
     };
-    cursor="rocket";
-    cursorAim="EmptyCursor";
-    cursorSize=0;
+    cursor = "rocket";
+    cursorAim = "EmptyCursor";
+    cursorSize = 0;
     class Far_AI: RocketPods {
-        displayName="Hydra";
-        burst=1;
-        reloadTime=0.079999998;
-        autoFire=0;
-        showToPlayer=0;
-        soundBurst=0;
-        dispersion=0.011;
-        minRange=150;
-        minRangeProbab=0.60000002;
-        midRange=600;
-        midRangeProbab=0.89999998;
-        maxRange=2500;
-        maxRangeProbab=0.12;
-        sounds[]= {
+        displayName = "Hydra";
+        burst = 1;
+        reloadTime = 0.079999998;
+        autoFire = 0;
+        showToPlayer = 0;
+        soundBurst = 0;
+        dispersion = 0.011;
+        minRange = 150;
+        minRangeProbab = 0.60000002;
+        midRange = 600;
+        midRangeProbab = 0.89999998;
+        maxRange = 2500;
+        maxRangeProbab = 0.12;
+        sounds[] = {
             "StandardSound"
         };
         class StandardSound {
-            weaponSoundEffect="DefaultRifle";
+            weaponSoundEffect = "DefaultRifle";
             begin1[] = {
                 "\rhsusf\addons\rhsusf_a2port_air\data\Sounds\RocketLauncher_Shot21",
                 3.2,
@@ -52,27 +52,27 @@ class rhs_weap_FFARLauncher: RocketPods {
         };
     };
     class Burst: RocketPods {
-        displayName="ALL";
-        burst=1;
-        salvo=2;
-        reloadTime=0.079999998;
-        soundContinuous=0;
-        autoFire=1;
-        aiDispersionCoefX=1.5;
-        aiDispersionCoefY=1;
-        dispersion=0.012;
-        textureType="fullAuto";
-        minRange=1;
-        minRangeProbab=0.001;
-        midRange=2;
-        midRangeProbab=0.001;
-        maxRange=3;
-        maxRangeProbab=0.001;
-        sounds[]= {
+        displayName = "ALL";
+        burst = 1;
+        salvo = 2;
+        reloadTime = 0.079999998;
+        soundContinuous = 0;
+        autoFire = 1;
+        aiDispersionCoefX = 1.5;
+        aiDispersionCoefY = 1;
+        dispersion = 0.012;
+        textureType = "fullAuto";
+        minRange = 1;
+        minRangeProbab = 0.001;
+        midRange = 2;
+        midRangeProbab = 0.001;
+        maxRange = 3;
+        maxRangeProbab = 0.001;
+        sounds[] = {
             "StandardSound"
         };
         class StandardSound {
-            weaponSoundEffect="DefaultRifle";
+            weaponSoundEffect = "DefaultRifle";
             begin1[] = {
                 "\rhsusf\addons\rhsusf_a2port_air\data\Sounds\RocketLauncher_Shot21",
                 3.2,
@@ -84,39 +84,39 @@ class rhs_weap_FFARLauncher: RocketPods {
                 1
             };
         };
-        soundBurst=0;
+        soundBurst = 0;
     };
     class Single: Burst {
-        displayName="1";
-        autoFire=0;
-        salvo=1;
-        textureType="semi";
+        displayName = "1";
+        autoFire = 0;
+        salvo = 1;
+        textureType = "semi";
     };
     class Two: Single {
-        displayName="2";
-        salvo=2;
-        burst=1;
+        displayName = "2";
+        salvo = 2;
+        burst = 1;
     };
     class Four: Single {
-        displayName="4";
-        salvo=2;
-        burst=2;
+        displayName = "4";
+        salvo = 2;
+        burst = 2;
     };
     class Eight: Four {
-        displayName="8";
-        burst=8;
+        displayName = "8";
+        burst = 8;
     };
     class Twelve: Four {
-        displayName="12";
-        burst=12;
+        displayName = "12";
+        burst = 12;
     };
     class TwentyFour: Four {
-        displayName="24";
-        burst=24;
+        displayName = "24";
+        burst = 24;
     };
 };
 class rhs_weap_FFARLauncher_M229: rhs_weap_FFARLauncher {
-    displayName="Hydra (M229 HEPD)";
+    displayName = "Hydra (M229 HEPD)";
     magazines[] = {
         "rhs_mag_M229_19",
         "rhs_mag_M229_19_green",
@@ -125,7 +125,7 @@ class rhs_weap_FFARLauncher_M229: rhs_weap_FFARLauncher {
     };
 };
 class rhs_weap_FFARLauncher_M257: rhs_weap_FFARLauncher {
-    displayName="Hydra (M257 ILLUM)";
+    displayName = "Hydra (M257 ILLUM)";
     magazines[] = {
         "rhs_mag_M257_7",
         "rhs_mag_M257_7_green",
@@ -136,11 +136,11 @@ class rhs_weap_FFARLauncher_M257: rhs_weap_FFARLauncher {
         "Single"
     };
     class Far_AI: Far_AI {
-        minRange=3000;
-        minRangeProbab=0.60000002;
-        midRange=4000;
-        midRangeProbab=0.89999998;
-        maxRange=6000;
-        maxRangeProbab=0.60000002;
+        minRange = 3000;
+        minRangeProbab = 0.60000002;
+        midRange = 4000;
+        midRangeProbab = 0.89999998;
+        maxRange = 6000;
+        maxRangeProbab = 0.60000002;
     };
 };
