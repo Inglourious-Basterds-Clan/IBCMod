17d656018333d1289fe55f33e5de72b6da79d7cd
kolmipilot <92938734+kolmipilot@users.noreply.github.com>
2026-09-12 21:54:08 +0200

    Add - Jca Infantry Arsenal Ace Compat/Fix (#41)
    * jca infantry arsenal fix
    
    * cleanup
    
    ---------
    
    Co-authored-by: MiszczuZPolski <71414303+MiszczuZPolski@users.noreply.github.com>

---
+++ addons/compat_jca/script_component.hpp
@@ -0,0 +1,18 @@
+#define COMPONENT compat_jca
+#define COMPONENT_BEAUTIFIED Compat JCA
+#include "\z\ibc\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define CBA_DEBUG_SYNCHRONOUS
+// #define ENABLE_PERFORMANCE_COUNTERS
+
+#ifdef DEBUG_ENABLED_COMPAT_JCA_
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_COMPAT_JCA
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_COMPAT_JCA
+#endif
+
+#include "\z\ibc\addons\main\script_macros.hpp"
---
+++ docs/addons/compat_jca/README.md
@@ -0,0 +1,12 @@
+# Compat JCA
+
+Dodaje ace compatibility do JCA Weapons
+
+## Cechy
+
+- Dodaje wsparcie dla ACE Advanced Ballistics
+- Usuwa ace_frag z Hand Flares
+
+## Autorzy
+
+- [kolmipilot](https://github.com/kolmipilot)
